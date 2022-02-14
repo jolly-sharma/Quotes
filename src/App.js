40b065b9cedbbfe2674f2ae86ee301133b7d6c0c
jolly sharma <jollykumari9168@gmail.com>
2022-02-14 12:21:55 +0530

    feat: Added new feature named Lazy Loading.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
+import React, { Suspense } from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 
-import AllQuotes from './pages/AllQuotes';
-import QuoteDetail from './pages/QuoteDetail';
-import NewQuote from './pages/NewQuote';
-import NotFound from './pages/NotFound';
 import Layout from './components/layout/Layout';
+import LoadingSpinner from './components/UI/LoadingSpinner';
+
+const NewQuote = React.lazy(() => import('./pages/NewQuote'));
+const QuoteDetail = React.lazy(() => import('./pages/QuoteDetail'));
+const NotFound = React.lazy(() => import('./pages/NotFound'));
+const AllQuotes = React.lazy(() => import('./pages/AllQuotes'));
 
 function App() {
   return (
     <Layout>
+      <Suspense 
+      fallback={ 
+      <div className='centered'> 
+      <LoadingSpinner />
+      </div>
+    }
+    >
       <Switch>
         <Route path='/' exact>
           <Redirect to='/quotes' />
@@ -26,6 +36,7 @@ function App() {
           <NotFound />
         </Route>
       </Switch>
+      </Suspense>
     </Layout>
   );
 }
